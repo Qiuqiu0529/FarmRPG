93a5d165390c9832184c8388d75255597006909f
Qiuqiu0529 <1711871800@qq.com>
2022-12-04 16:48:07 +0800

    1

--- src/Mgr/GameMgr.java
+++ src/Mgr/GameMgr.java
@@ -1,9 +1,22 @@
 package Mgr;
 import Player.MoneyPresenter;
+
+import java.sql.Time;
+import java.util.List;
+
+import Choice.ChoiceMoveToFarmLand;
+import Choice.ChoiceMoveToForest;
+import Choice.DefaultChoice;
+import Choice.IChoice;
 import Player.EnergyPresenter;
 
 public class GameMgr {
     private static volatile GameMgr instance;
+    static List<IChoice> choices;
+    static List<IChoice> realchoices;
+    static int day=1;
+    enum TimePeriod{Morning,Afternoon,Night,SleepTime};
+    TimePeriod timePeriod;
 
     private GameMgr() {
         if (instance != null) {
@@ -11,6 +24,8 @@ public class GameMgr {
         }
         else
         {
+            System.out.println("第一天开始");
+            timePeriod=TimePeriod.Morning;
             RunGame();
         }
         
@@ -44,8 +59,42 @@ public class GameMgr {
         status = GameStatus.STOPPED;
     }
 
+    public void InitChoices()
+    {
+        DefaultChoice defaultChoice=new DefaultChoice();
+        ChoiceMoveToFarmLand choiceMoveToFarmLand=new ChoiceMoveToFarmLand();
+        ChoiceMoveToForest choiceMoveToForest=new ChoiceMoveToForest();
+        choices.add(defaultChoice);
+        choices.add(choiceMoveToFarmLand);
+        choices.add(choiceMoveToForest);
+    }
+
+
+
+
+    public void DisplayChoice()
+    {
+        realchoices.clear();
+        for (IChoice iChoice : choices) {
+            if(iChoice.CanChoose())
+            {
+                realchoices.add(iChoice);
+            }
+        }
+        for(int i=0;i<realchoices.size();++i)
+        {
+            System.out.print("选择"+Integer.toString(i));
+            realchoices.get(i).ChoiceInfo();
+        }
+        
+    }
+
+
+
     public void EndADay()
     {
+        MoneyPresenter.GetInstance().CheckDebt();
+
 
     }
 }
